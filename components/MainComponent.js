76c4f8f6ff3972e00fb72c11699c0960332e1cf9
Narayanan Ammanur <nammanur@gmail.com>
2022-04-16 18:38:23 +0530

    Redux Adding Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -9,6 +9,7 @@ import { createDrawerNavigator, DrawerItems  } from 'react-navigation-drawer';
 import CampsiteInfo from './CampsiteInfoComponent';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
+import Reservation from './ReservationComponent';
 import { Icon } from 'react-native-elements';
 import SafeAreaView from 'react-native-safe-area-view';
 
@@ -126,6 +127,29 @@ const ContactNavigator = createStackNavigator(
     }
 );
 
+const ReservationNavigator = createStackNavigator(
+    {
+        Reservation: { screen: Reservation },
+    }, 
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='tree'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
+
 const CustomDrawerContentComponent = props => (
     <ScrollView>
         <SafeAreaView 
@@ -171,6 +195,33 @@ const MainNavigator = createDrawerNavigator(
                     />
                 )
             }},
+            Reservation: {screen: ReservationNavigator,
+                screen:'ReservationNavigator',
+                navigationOptions: {
+                    drawerLabel:'Reserve Campsite',
+                    drawerIcon: ({tintColor}) => (
+                        <Icon
+                            name='tree'
+                            type='font-awesome'
+                            size={24}
+                            color={tintColor}
+                        />
+                    )
+                }},
+            Reservation: {
+            screen: ReservationNavigator,
+            navigationOptions: {
+                drawerLabel: 'Reserve Campsite',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='tree'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
             About: {
                 screen: AboutNavigator,
                 navigationOptions: {
